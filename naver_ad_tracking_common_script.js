bfce907c0310a7507f76f35774b131170e7bd936
Minjeong Koo <cookie00421@gmail.com>
2021-01-13 10:29:14 +0900

    Update naver_ad_tracking_common_script.js

--- naver_ad_tracking_common_script.js
+++ naver_ad_tracking_common_script.js
@@ -1,5 +1,4 @@
 <!-- naver ad tracking common script -->
-<script type="text/javascript" src="//wcs.naver.net/wcslog.js"> </script> 
 <script type="text/javascript"> 
 if (!wcs_add) var wcs_add={};
 wcs_add["wa"] = "s_1510969669f5";
